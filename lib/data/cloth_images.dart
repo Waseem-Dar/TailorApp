6dade9c9f3c8363707d25d783e9e2a34fff735b0
waseem <waseemws456@gmail.com>
2025-02-17 18:00:25 +0500

    add choicechip in gallery screen for filtering

---
+++ lib/data/cloth_images.dart
@@ -0,0 +1,32 @@
+class ClothImages {
+
+  static List<String>  CarouselSliderImages =[
+
+
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiYAIVWqO7k1hL5Te51EdtSQMHYoukr3QI3oJx7lHTTUfUQxqOwDZGZlUkHhYzH1FjvaY&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZgxFoZmx-pTYw7p1FqAgp0Kb04cXrR6EXvg&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYWv4ch-t-KNKoKDxHj6ACWexYkGpb2nDatXtkfK3UQYIaQZY7g2aZuPKchCgBYKIC4r4&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOAVKuG9WuukxkyU9SLDdWzqm3sXKo753mBqbPoGvWfHpWaemex1v2erVa9aK_NqX439U&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPW-1DtUMFwJgs2UFBP0ezbW-9OZhNccuCpA&s",
+  ];
+
+
+
+  static List<String> GridViewImages = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXjSiZ27i7K-juLbdWFC3LrGl_DfOnipCzfg&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfizfNH82NUz0xTyjTn_tc7ToBHHmta_7wbg&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP7K6MODAba0a31xRAsPym-QTYYLZVJFm9Mg&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnXz2I_eQj_KbBYzJdJFoyVubCG71x7-Xm-g&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKxHwDF_ZjGD9rRxZ-4ETEiRGX3sBgWiK23w&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3WnGaxJkNeJx0z_UJtbOOwEsvsltcxByN2ttMgeFX-WKEuXqAmKkqgf0gu6kvDxRy290&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpQUHB5JUsh34fNEG5C-JNY3mMg0TwXzkoW8Bih553yqCmb9yxcmbZ_uYNaAxc0Oxb_zY&usqp=CAU",
+    "https://99designs-start-attachments.imgix.net/alchemy-pictures/2020%2F04%2F01%2F18%2F14%2F42%2F053ebff6-4ec0-4812-bf49-f61a9656983d%2Fapparel-design.jpg?auto=format&ch=Width%2CDPR&crop=false&fm=pjpg&q=25&w=900&h=900",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLjiE56f_k0vHnkkoui5ADWvC6yhF5edxOMwt-b_Igj8d0zento1QrhKGXi-Ai_R1_uXo&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-fBxFyoxz54hB0FgzPZrLiw2-iIBMI0sMK0cnW5c0tttyJZBhs17IhY0PE1q-69iz5HE&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUaXJKJMZ5xQHqW-MyF_QhTPGF_7r13XNyIzZG2e1nmR7-oABWZRpxhx188y9KiyaRjOE&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDpCLXb9oGhOjsVOBL3H450g13VJZ5nCaTiF-s0MiZn8yp6QVNySSbbO3ovgmUfooYVA8&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgoPfGOa7pvT48MtdAtP-cm1-xV31MTC6kjLCNhDTovRGvTmuAH9V_tfUtyykYBDgdfw0&usqp=CAU",
+  ];
+
+
+}
