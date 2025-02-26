773037554b84633ee8c9662d2df3eaa98dfa465a
waseem <waseemws456@gmail.com>
2025-02-26 18:31:14 +0500

    Add selection into client list to delete multiple users

--- lib/view/tabs/users_screen.dart
+++ lib/view/tabs/users_screen.dart
@@ -14,16 +14,34 @@ class UsersScreen extends StatefulWidget {
   @override
   State<UsersScreen> createState() => _UsersScreenState();
 }
-List   list = [1,2,3,4,5,6,7,8,9,9,10,11,12,13,14,15];
 
 class _UsersScreenState extends State<UsersScreen> {
+  List list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11, 12, 13, 14, 15];
+
+  Set<int> selectedItems = {}; // Stores selected item indexes
+  bool isSelectionMode = false;
+
+  void toggleSelection(int index) {
+    setState(() {
+      if (selectedItems.contains(index)) {
+        selectedItems.remove(index);
+        if (selectedItems.isEmpty)
+          isSelectionMode = false; // Exit selection mode if none selected
+      } else {
+        selectedItems.add(index);
+        isSelectionMode = true; // Activate selection mode
+      }
+    });
+  }
+
   bool isSearch = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
         appBar: AppBar(
           backgroundColor: AppColors.primaryColor2,
-          surfaceTintColor: Colors.transparent,
+          // surfaceTintColor: Colors.transparent,
           title: Text(
             "Stitch Craft",
             style: GoogleFonts.poppins(color: AppColors.white),
@@ -75,96 +93,141 @@ class _UsersScreenState extends State<UsersScreen> {
         ),
         body: ListView.builder(
           itemCount: list.length,
+          padding: EdgeInsets.symmetric(vertical: 4),
           itemBuilder: (context, index) {
+            bool isSelected = selectedItems.contains(index);
             return Dismissible(
-              key:  Key(index.toString()),
+              key: Key(index.toString()),
               background: Container(
-                color:  AppColors.red.withOpacity(0.73),
+                color: AppColors.red.withOpacity(0.73),
                 padding: EdgeInsets.only(right: 20),
                 alignment: Alignment.centerRight,
-                child: Icon(Icons.delete,color: AppColors.white,size: 35,),
+                child: Icon(
+                  Icons.delete,
+                  color: AppColors.white,
+                  size: 35,
+                ),
               ),
-              direction: DismissDirection.endToStart,
+              direction: !isSelectionMode
+                  ? DismissDirection.endToStart
+                  : DismissDirection.none,
               confirmDismiss: (DismissDirection direction) async {
-                return await showDeleteDialog(context,index);
+                return await showDeleteDialog(context, index);
               },
-              child: Column(
-                children: [
-                  ListTile(
-                    tileColor: AppColors.background,
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => UserDetailsScreen(),));
-                    },
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 5, horizontal: 16),
-                    leading: Container(
-                      height: 57,
-                      width: 57,
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        color: AppColors.grey2.withOpacity(0.2),
-                        shape: BoxShape.circle,
-                      ),
-                      child: CachedNetworkImage(
-                        imageUrl:
-                            "https://encrypted-tbn0.gstatic.om/images?q=tbn:ANd9GcQO1kJt8LJRx4M1ZiMX0p3x7ScJ0vJzizJeAoPw-NY4LXQUvYbhYetcqweLkHUyI1CqJKs&usqp=CAU",
-                        fit: BoxFit.cover,
-                        placeholder: (context, url) => Padding(
-                            padding: EdgeInsets.all(10),
-                            child: CircularProgressIndicator()),
-                        errorWidget: (context, url, error) => Icon(
-                          Icons.person,
-                          color: AppColors.grey2,
-                          size: 45,
-                        ),
-                      ),
-                    ),
-                    title: Padding(
-                      padding: const EdgeInsets.only(bottom: 5),
-                      child: Text("Sabir Saleem",style: GoogleFonts.poppins(fontSize: 15,fontWeight: FontWeight.w600,color: Colors.black),),
-                    ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                child: ListTile(
+                  onLongPress: () => toggleSelection(index),
+                  onTap: isSelectionMode
+                      ? () => toggleSelection(index)
+                      : () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => UserDetailsScreen(),
+                              ));
+                        },
+                  tileColor: isSelected
+                      ? AppColors.primaryColor2.withOpacity(0.15)
+                      : AppColors.background,
 
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('This is a open source and free UI library.',style: GoogleFonts.poppins(fontWeight: FontWeight.w400,fontSize: 12,color:const Color(0xFF6C6C6C) ),maxLines: 1,overflow: TextOverflow.ellipsis,),
-                        Text('$index',style: GoogleFonts.poppins(fontWeight: FontWeight.w400,fontSize: 12,color:const Color(0xFF6C6C6C) ),maxLines: 1,),
-                      ],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                  leading: Container(
+                    height: 57,
+                    width: 57,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      color: AppColors.grey2.withOpacity(0.2),
+                      shape: BoxShape.circle,
                     ),
-                    trailing:  Icon(
-                      Icons.arrow_forward_ios,
-                      size: 18,
+                    child: CachedNetworkImage(
+                      imageUrl:
+                          "https://encrypted-tbn0.gstatic.om/images?q=tbn:ANd9GcQO1kJt8LJRx4M1ZiMX0p3x7ScJ0vJzizJeAoPw-NY4LXQUvYbhYetcqweLkHUyI1CqJKs&usqp=CAU",
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) => Padding(
+                          padding: EdgeInsets.all(10),
+                          child: CircularProgressIndicator()),
+                      errorWidget: (context, url, error) => Icon(
+                        Icons.person,
+                        color: AppColors.grey2,
+                        size: 45,
+                      ),
                     ),
                   ),
-                  Divider(
-                    thickness: 0,
-                    height: 0,
-                  )
-                ],
+                  title: Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: Text(
+                      "Sabir Saleem",
+                      style: GoogleFonts.poppins(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'This is a open source and free UI library.',
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                            color: const Color(0xFF6C6C6C)),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        '$index',
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                            color: const Color(0xFF6C6C6C)),
+                        maxLines: 1,
+                      ),
+                    ],
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                  ),
+                ),
               ),
             );
           },
         ));
   }
 
-  Future showDeleteDialog(BuildContext context, int index)async{
+  Future showDeleteDialog(BuildContext context, int index) async {
     return await showDialog<bool>(
       context: context,
       builder: (BuildContext context) {
-        return  AlertDialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
           backgroundColor: Colors.white,
           surfaceTintColor: Colors.white,
           insetPadding: const EdgeInsets.symmetric(horizontal: 30),
-          contentPadding: const EdgeInsets.only( right: 24,left: 24,top: 24,bottom: 0),
+          contentPadding:
+              const EdgeInsets.only(right: 24, left: 24, top: 24, bottom: 0),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text("Are you sure you want to remove this address/location?",
-                style: GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.w400,color: Colors.black),textAlign: TextAlign.center,),
-              const SizedBox(height: 15,),
+              Text(
+                "Are you sure you want to remove this address/location?",
+                style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
               Divider(
-                height:1,
+                height: 1,
                 color: const Color(0xFF939393).withOpacity(0.29),
               ),
             ],
@@ -173,18 +236,37 @@ class _UsersScreenState extends State<UsersScreen> {
           actionsAlignment: MainAxisAlignment.spaceEvenly,
           actions: [
             TextButton(
-                onPressed: (){
-                  Navigator.pop(context,true);
-                }, child: Text("Yes",style: GoogleFonts.poppins(fontWeight: FontWeight.w500,fontSize: 15,color: AppColors.red),)),
-            SizedBox(height:50,child: VerticalDivider(color: const Color(0xFF939393).withOpacity(0.29),width: 1,endIndent: 15,)),
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+                child: Text(
+                  "Yes",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                      color: AppColors.red),
+                )),
+            SizedBox(
+                height: 50,
+                child: VerticalDivider(
+                  color: const Color(0xFF939393).withOpacity(0.29),
+                  width: 1,
+                  endIndent: 15,
+                )),
             TextButton(
-                onPressed: (){
-              Navigator.pop(context,false);
-            }, child: Text("No",style: GoogleFonts.poppins(fontWeight: FontWeight.w500,fontSize: 15,color: const Color(0xFF5C90DC)),))
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+                child: Text(
+                  "No",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                      color: const Color(0xFF5C90DC)),
+                ))
           ],
         );
       },
     );
   }
-
 }
